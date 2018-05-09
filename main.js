8774df316d3507d0e61ea704992226f2907974f6
YashaShiro <38774190+YashaShiro@users.noreply.github.com>
2018-05-09 01:30:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -72,6 +72,7 @@ var saltoPj2;
           pj2.body.collideWorldBounds = true;
           //ANIMACIONES
           pj.animations.add('quieto', [18,19,20,21,22,23,24],10,false);
+          pj.animations.add('principal', [18],10,false);
           pj.animations.add('correr', [33,34,35,36,37,38,39,40],10,false);
           pj.animations.add('correrAtras', [40,39,38,37,36,35,34,33],10,false);
           pj.animations.add('ataque', [41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56],15,false);
@@ -79,7 +80,8 @@ var saltoPj2;
           pj.animations.add('agachado',[26],10,false);
           pj.animations.add('victoria',[57,58,59,60,61],7,false);
           pj.animations.add('ataque2',[27,28,29,30,31,32,18],7,false);
-          pj2.animations.add('daño',[0,1,2,3,4,5,6,78],7,false);    
+          pj2.animations.add('daño',[0,1,2,3,4,5,6,78],7,false);   
+          pj.animations.play('principal');
                
 
         }
@@ -117,7 +119,7 @@ var saltoPj2;
             if(key.keyCode === Phaser.KeyCode.X && moverse == true && atacar == true){
               patada = true;
               seguir = true;
-              pj.animations.play('ataque');
+              pj.animations.play('ataque2');
               moverse = false;
               setTimeout(dd,1500);
               pj2.animations.play('daño');
