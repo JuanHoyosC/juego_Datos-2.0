22529f0906fc2dc26411813955bfeac42303e6d9
YashaShiro <38774190+YashaShiro@users.noreply.github.com>
2018-05-09 01:55:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,9 +26,9 @@ var agachado = false;
       function preload(){
 
             juego.load.image('Fondo', 'Imagenes/Fondo_Juego.jpg');
-            juego.load.spritesheet('personajes', 'Quieto/PJ.png',200,165,62)
-            juego.load.spritesheet('golpe', 'Imagenes/golpe.png',290,400)
-            juego.load.spritesheet('enemigo', 'Quieto/Personajes.png',160,160)
+            juego.load.spritesheet('personajes', 'Quieto/PJ.png',200,165,62);
+            juego.load.spritesheet('golpe', 'Imagenes/golpe.png',290,400);
+            juego.load.spritesheet('enemigo', 'Quieto/PJ.png',200,165,62);
             juego.load.audio('audio1','Musica/GameOver.mp3');
         }
 
@@ -70,16 +70,18 @@ var agachado = false;
           //ANIMACIONES
           pj.animations.add('quieto', [18,19,20,21,22,23,24],10,false);
           pj.animations.add('principal', [18],10,false);
+          pj2.animations.add('principal', [18],10,false);
           pj.animations.add('correr', [33,34,35,36,37,38,39,40],10,false);
           pj.animations.add('correrAtras', [40,39,38,37,36,35,34,33],10,false);
           pj.animations.add('ataque', [41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56],15,false);
-          pj2.animations.add('quieto', [0,1,2,3,4,5,6,7],10,false);
+          pj2.animations.add('quieto', [18,19,20,21,22,23,24],10,false);
           pj.animations.add('agachado',[26],10,false);
           pj.animations.add('victoria',[57,58,59,60,61],7,false);
           pj.animations.add('ataque2',[27,28,29,30,31,32,18],7,false);
-          pj.animations.add('ataque3',[9,10,11,12,13,14,15,16,17],7,false);
+          pj.animations.add('ataque3',[9,10,11,12,13,14,15,16,17,18],7,false);
           pj2.animations.add('daño',[0,1,2,3,4,5,6,78],7,false);   
           pj.animations.play('principal');
+          pj2.animations.play('principal');
                
 
         }
